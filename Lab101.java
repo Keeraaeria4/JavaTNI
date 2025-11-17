09fbb60d5288846d035a317c9d1625a9069c7bb7
Keeraaeria4 <xzxasxpoke@gmail.com>
2025-11-17 17:23:49 +0700

    Add files via upload

---
+++ Lab101.java
@@ -0,0 +1,9 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Tanatchaporn Srimongkol");
+        //System.out.println("\"INT-108\" \'Objext-Oriented Programming\'");
+        System.out.println("\tThis is my first time learning Java");
+        System.out.println("\tI hope to get grade \"A\"");
+
+    }
+}
